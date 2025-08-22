a1b79cd4941b6fe5e8ce4dbcbc025b43bd0ec249
THALES VALE DA SILVA <25162457@edu119.local>
2025-08-22 16:14:05 -0300

    Adicionando o POST

--- server.js
+++ server.js
@@ -95,7 +95,19 @@ app.get("/alunos/cor/:cor", (req, res)=>{
     }
 })
 
+app.post("/alunos", (req, res)=> {
+    const {nome, cor, idade} = req.body;
 
+    if(!nome || !cor || !idade){
+        return res.status(400).json({msg: "nome cor e idade são obrigados"})
+}
+    const id = ALUNOS.length > 0 ? ALUNOS[ALUNOS.length - 1].id + 1 : 1
+    const novoAluno = {id, nome, cor, idade}
+
+    console.log(novoAluno)
+    ALUNOS.push(novoAluno)
+    res.status(201).json({msg: "Aluno criado com sucesso"})
+})
 
 app.listen(PORT, ()=>{
     console.log(`Servidor rodando em http://localhost:${PORT}`)
